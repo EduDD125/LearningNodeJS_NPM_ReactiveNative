93f4388a548cb68af7b9ae06b58cd51bc99f7e4f
Eduardo Derisso <dudu.derisso@gmail.com>
2024-07-30 21:47:10 -0300

    feat[routes]: creating some kind of logic processing of the data recived front que front-end request

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const routes = express.Router();
 
-routes.post('/login', (resquest, response) => {
-    response.send("login");
+routes.post('/login', (request, response) => {
+   //response.send("login");
+   const {email, password} = request.body;
+
+    response.send(email + " || " + password);
+    if (password == "123456")
+        console.log("senha correta")
+
 });
 
 module.exports = routes;
