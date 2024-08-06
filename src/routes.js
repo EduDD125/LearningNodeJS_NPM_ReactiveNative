ddedf190d6640c9e23a48a5ec27db2fa293f5637
Eduardo Derisso <dudu.derisso@gmail.com>
2024-08-06 19:22:05 -0300

    feat[routes]: criado arquivo de rotas para responder a direntes requisições https

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ routes.post('/login', (request, response) => {
    //response.send("login");
    const {email, password} = request.body;
 
-    response.send(email + " || " + password);
+    response.send(email + " |dfndlkfjdlskafjlkdsajfl " + password);
     if (password == "123456")
         console.log("senha correta")
 
