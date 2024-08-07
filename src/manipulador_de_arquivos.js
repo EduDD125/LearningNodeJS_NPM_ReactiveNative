c76e7432ae08635e4382ed735386b6f0c716d3ae
Eduardo Derisso <dudu.derisso@gmail.com>
2024-08-06 21:54:13 -0300

    feat[maninupulador_de_arquivos]: modularizado a funcionalidade de escrita de arquivos com textos enviados por cliente por meio de requisição http

--- src/manipulador_de_arquivos.js
+++ src/manipulador_de_arquivos.js
@@ -4,9 +4,27 @@ const arquivos = express.Router();
 const fileSystem = require("fs");
 
 //leitura do conteudo do arquivo
-fileSystem.readFile("../exemplo.txt", "utf8", (err, data) =>{
+/*fileSystem.readFile("../exemplo.txt", "utf8", (err, data) =>{
     if(err) throw err;
     console.log(data);
-})
+})*/
+
+arquivos.post("/escrever", (req, res) => {
+    const {content} = req.body; // Pegando a string do query parameter 'content'
+    const stringToWrite = content;
+
+
+    if (!stringToWrite) {
+        return res.status(400).send("O parâmetro 'content' é obrigatório.");
+    }
+
+    fs.writeFile('Novo.txt', stringToWrite, (err) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send("Erro ao escrever no arquivo.");
+        }
+        res.send("Conteúdo escrito no arquivo com sucesso!");
+    });
+});
 
 module.exports = arquivos;
--- src/server.js
+++ src/server.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const routes = require("./routes");
+const arquivos = require("./manipulador_de_arquivos");
+const fs = require('fs');
 
 const app = express();
 
-app.use((express.json()));
+app.use(express.json()); // Middleware para interpretar o JSON
+
 app.use(routes);
 
 app.get('/', (request, response) => {
     response.send("Olá Mundo");
 });
 
+
+
 app.listen(3000, () => {
-    console.log("conectando a porta padrao - 3000")
+    console.log("conectando a porta padrão - 3000");
 });
